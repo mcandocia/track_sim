fixed calculation bugs

group_clusters.py
@@ -4,6 +4,7 @@ import json
 import numpy as np
 from utility import Clogger
 import pandas as pd
+import re
 
 logger = Clogger('group_clusters.log')
 
@@ -119,7 +120,7 @@ class Group:
                 v-1 for v in
                 Counter([m['filename'] for m in self.members]).values()
             ]),
-            'long_raster_size': c.RASTER_SIZE_LAT * np.cos(
+            'long_raster_size': c.RASTER_SIZE_LAT / np.cos(
                 np.median([
                     np.mean(m['bbox']['lat'])
                     for m in self.members
@@ -127,8 +128,10 @@ class Group:
             ),
         }
 
-    def to_df(self):
-        return pd.DataFrame([self.summary()])
+    def to_df(self, group_id=-1):
+        summary = self.summary()
+        summary['group_id'] = group_id
+        return pd.DataFrame([summary])
 
     def pairwise_iter(self):
         n_groups = len(self.members)
@@ -144,16 +147,23 @@ class Group:
 
     def make_raster_dfs(
             self,
-            group_idx=-1,
-            directional=False
+            group_id=-1,
+            directional=False,
+            options={}
     ):
         df_list = []
         rd_df_list = []
 
+        if options.get('truncate_file_path'):
+            fn_trans = lambda x: re.sub('.*/','', x)
+        else:
+            fn_trans = lambda x: x
+
         member_id_df = pd.DataFrame([
             {
                 'member_id': i,
-                'filename': member['filename']
+                'filename': fn_trans(member['filename']),
+                'group_id': group_id,
             }
             for i, member in enumerate(self.members)
         ])
@@ -161,7 +171,7 @@ class Group:
         for i, member in enumerate(self.members):
             df = member['rasterization'].copy()
             df['member_id'] = i
-            df['group_id'] = group_idx
+            df['group_id'] = group_id
             df_list.append(df)
 
             if directional:
@@ -175,7 +185,7 @@ class Group:
                     directional=directional
                 )
             rd_df['member_id'] = i
-            rd_df['group_id'] = group_idx
+            rd_df['group_id'] = group_id
             rd_df_list.append(rd_df)
             
 
@@ -333,7 +343,7 @@ class GroupProcessor:
                 )
         return all_pairs
 
-    def groups_to_df_dict(self, directional=False):
+    def groups_to_df_dict(self, directional=False,options={}):
         member_dataframes = []
         group_dataframes = []
         member_id_dataframes = []
@@ -342,8 +352,9 @@ class GroupProcessor:
         for i, group in enumerate(self.groups):
             
             member_df_dict = group.make_raster_dfs(
-                group_idx=i,
+                group_id=i,
                 directional=directional,
+                options=options,
             )
 
             member_dataframes.append(
@@ -360,7 +371,7 @@ class GroupProcessor:
             
 
             group_dataframes.append(
-                group.to_df()
+                group.to_df(group_id=i)
             )
 
         #print(member_dataframes)            

tracksim.py
@@ -268,7 +268,7 @@ def get_metadata(record, fn):
     median_lat = np.median(record['position_lat'])
     cosine_lat = np.cos(PI/180 * median_lat)
 
-    bbox_increase_long = c.BBOX_INCREASE_LAT * cosine_lat
+    bbox_increase_long = c.BBOX_INCREASE_LAT / cosine_lat
     
     bbox = {
         'lat': [
@@ -306,7 +306,7 @@ def add_directions(data, options):
         diffs = diffs.dropna().reset_index().append(
             diffs.iloc[[-1] * c.ANGLE_LAG_SECONDS].reset_index()
         ).reset_index()
-        diffs['position_long'] = diffs['position_long'] * lon_lat_ratio
+        diffs['position_long'] = diffs['position_long'] / lon_lat_ratio
         record['angle'] = np.arctan2(
             diffs['position_lat'],
             diffs['position_long']
@@ -393,7 +393,7 @@ def write_grouper_to_disk(grouper, options, directional=False):
             'Writing data from grouper to disk. Concatenating first, which may take a '
             'while. Directional: %s' % directional
         )
-        df_dict = grouper.groups_to_df_dict(directional=directional)
+        df_dict = grouper.groups_to_df_dict(directional=directional, options=options)
         prefix = options['rasterized_output_prefix']
         if directional:
             prefix = '%s_directional' % prefix